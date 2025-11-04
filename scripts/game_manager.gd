24157dac96e21f6704e19cb9313e3693f7a6e186
amram <amramos0828@gmail.com>
2025-11-04 11:54:49 -0500

    chore: remove unit code from game manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -38,32 +38,3 @@ func spawn_unit(hex_pos: Vector2i, team: String, unit_name: String):
 	units.append(unit)
 	
 	print("Spawned ", unit_name, " at hex ", hex_pos)
-
-## Inner class for units
-#class UnitNode extends Node2D:
-	#var team: String = "RED"
-	#var hex_position: Vector2i
-	#var unit_size: float = 32.0
-	#var game_manager: Node = null
-	#
-	#func _ready():
-		#queue_redraw()
-	#
-	#func _draw():
-		#var color = Color.RED if team == "RED" else Color.BLUE
-		#var border_color = Color.WHITE
-		#
-		## Draw filled square
-		#var rect = Rect2(-unit_size/2, -unit_size/2, unit_size, unit_size)
-		#draw_rect(rect, color)
-		#draw_rect(rect, border_color, false, 2.0)
-	#
-	#func _input(event):
-		#if event is InputEventMouseButton:
-			#if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-				#var global_mouse = get_global_mouse_position()
-				#var local_pos = to_local(global_mouse)
-				#var rect = Rect2(-unit_size/2, -unit_size/2, unit_size, unit_size)
-				#if rect.has_point(local_pos):
-					#print("Unit: ", name, " | Team: ", team, " | Hex Position: ", hex_position)
-					#get_viewport().set_input_as_handled()
