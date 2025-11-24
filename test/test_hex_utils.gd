4416345be6b5ea6fb57d310e370bb9d53f566790
amram <amramos0828@gmail.com>
2025-11-24 08:58:31 -0500

    refac: first test

--- test/test_hex_utils.gd
+++ test/test_hex_utils.gd
@@ -1,12 +1,7 @@
 extends GdUnitTestSuite
 
-func test_HexUtils_hex_distance():
-	# given an origin and destination, they are the same
-	var origin = Vector2i(0, 0)
-	var destination = origin
-	
-	# get the distance between origin and destination
-	var distance = HexUtils.hex_distance(origin, destination)
-	
-	# the result should be 0
-	assert_int(distance).is_equal(0)
+## Test: Distance from hex to itself should be 0
+func test_hex_distance_same_hex():
+	assert_int(HexUtils.hex_distance(Vector2i(0, 0), Vector2i(0, 0))).is_equal(0)
+	assert_int(HexUtils.hex_distance(Vector2i(5, 5), Vector2i(5, 5))).is_equal(0)
+	assert_int(HexUtils.hex_distance(Vector2i(10, 10), Vector2i(10, 10))).is_equal(0)
