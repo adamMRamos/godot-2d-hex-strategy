5cb2ecd5f97edff307be380f324c7b115477c7c7
amram <amramos0828@gmail.com>
2025-11-24 21:31:27 -0500

    test: max movement

--- test/test_unit_controller.gd
+++ test/test_unit_controller.gd
@@ -56,8 +56,52 @@ func test_can_move_no_movement():
 	unit.current_movement = 0
 	assert_bool(unit.can_move()).is_false()
 
-## Test: can_move returns false when current_movement is 0
+## Test: can_move returns false when current_movement is -1
 func test_can_move_negative_movement():
 	unit.current_movement = -1
 	assert_bool(unit.can_move()).is_false()
+
+## Test: use_movement sets current_movement to 0
+func test_use_movement_zeros_current():
+	unit.current_movement = 2
+	unit.use_movement()
+	assert_int(unit.current_movement).is_equal(0)
+
+## Test: use_movement works from any starting movement value
+func test_use_movement_from_various_values():
+	unit.current_movement = 5
+	unit.use_movement()
+	assert_int(unit.current_movement).is_equal(0)
+
+## Test: reset_movement restores current_movement to max_movement
+func test_reset_movement_restores_current():
+	unit.max_movement = 3
+	unit.current_movement = 0
+	unit.reset_movement()
+	assert_int(unit.current_movement).is_equal(3)
+
+## Test: reset_movement works with default max_movement
+func test_reset_movement_default():
+	unit.current_movement = 0
+	unit.reset_movement()
+	assert_int(unit.current_movement).is_equal(2)
+
+## Test: set_max_movement updates max_movement
+func test_set_max_movement_updates_max():
+	unit.set_max_movement(5)
+	assert_int(unit.max_movement).is_equal(5)
+
+## Test: set_max_movement updates current_movement
+func test_set_max_movement_updates_current():
+	unit.set_max_movement(4)
+	assert_int(unit.current_movement).is_equal(4)
+
+## Test: set_max_movement with different values
+func test_set_max_movement_various_values():
+	unit.set_max_movement(1)
+	assert_int(unit.max_movement).is_equal(1)
+	assert_int(unit.current_movement).is_equal(1)
 	
+	unit.set_max_movement(10)
+	assert_int(unit.max_movement).is_equal(10)
+	assert_int(unit.current_movement).is_equal(10)
